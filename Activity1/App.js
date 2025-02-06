617b6e20bd3e356bdf0c184c3a8a55c8980066c0
ckt1602 <139831110+ckt1602@users.noreply.github.com>
2025-02-06 16:57:38 +0800

    ScrollView

---
+++ Activity1/App.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import {Text,ScrollView} from 'react-native';
+
+const App =() => (
+    <ScrollView>
+      <Text>Akala ko'y habang-buhay tayo
+Akala ko'y hanggang dulo
+Kay haba pa ng kalsada
+Dito na ba tayo bababa?
+Kung ganito na nga ba'ng usapan
+Kung dito na ang hangganan
+Dapat sigurong iwasan
+Ang mga minsang kamustahan
+Mga nakasanayan
+Dapat nang kalimutan
+Upang 'di tayo magkasakitan
+Hanggang dito na lang
+Hanggang dito na lang
+Ikaw ba ang nagbago o ako, o tayo?
+Baka tayo?
+Hanggang dito na lang
+Hanggang dito na lang
+Kung tunay ang paalam
+'Wag ka nang magparamdam
+Dahil humihirap lang
+Hanggang dito na lang
+Akala ko'y habang-buhay ang awit
+Akala ko'y hanggang langit
+Kay haba pa ng kantahan
+Dito na ba tayo tatantan?
+Kung ganito na nga ba'ng tadhana
+Sarang pinto at bintana
+Dapat sigurong iwasan
+Ang pagkatok sa 'ting nakaraan
+Mga nakaugalian
+Dapat nang pagbawalan
+Sunugin na ang mga larawan
+Hanggang dito na lang
+Hanggang dito na lang
+Ikaw ba ang nagbago o ako, o tayo?
+Baka tayo
+Hanggang dito na lang
+Hanggang dito na lang
+Kung tunay ang paalam
+'Wag ka nang magparamdam
+Dahil humihirap lang
+Hanggang dito na lang
+
+Hindi Tayo Pwede
+Song by
+The Juans
+Pilit nating iniwasan
+Ganitong mga tanungan
+At kahit 'di sigurado
+Tinuloy natin ang ating ugnayan
+Ngayo'y naubos na'ng kwentuhan
+Nagsimula nang magsisihan
+Lahat ay parang lumabo
+'Di alam kung sa'n tutungo
+Sabi ko na nga ba, dapat no'ng una pa lamang
+'Di na umasa, 'di naniwala
+Hindi tayo pwede
+Pinagtagpo pero 'di tinadhana
+Hindi na posible
+Ang mga puso'y huwag nating pahirapan
+Suko na sa laban
+Hindi tayo pwede
+Kay bigat na ng damdamin
+Bakit 'di pa natin aminin
+Dahil sa una pa lamang
+Alam nating wala tayong laban
+Sabi ko na nga ba, dapat no'ng una pa lamang
+'Di na umasa, 'di naniwala
+Hindi tayo pwede
+Pinagtagpo pero 'di tinadhana
+Hindi na posible
+Ang mga puso'y huwag nating pahirapan
+Suko na sa laban
+Hindi tayo pwede
+Hindi tayo pwede dahil una pa lang
+Alam naman nating mayroong hangganan
+At kahit ipilit, hanggang dito na lang
+Dito na lang
+Hindi tayo pwede
+Pinagtagpo pero 'di tinadhana
+Hindi na posible
+Ang mga puso'y huwag nating pahirapan
+Suko na sa laban
+Hindi tayo pwede
+Hindi tayo pwede
+Paboritong Pagkakamali
+Song by
+Audereus, Nateman, sixzee
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino ('titino)
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom?
+Ah, tawag sa 'yo, "Kabit", ako naman sabik
+Na madama 'yong yakap mo, presensya mo't halik
+"Paboritong pagkakamali" tawag sa 'yo
+Sabi mo, mga kaibigan ko, ayaw sa 'yo
+Gusto nila sa may magara ka na kotse
+Lakbay sa lugar na kailangan ng pasaporte
+Habang ako ay magdamag na nakaposte
+Sa aking hood, kasi hindi pa ako boss, eh
+Nahumaling ka sa aking pagkanta
+Panay ang tanong mo sa 'kin, "Nasa'n ka?"
+Sabi ko, "Sa bahay," kahit gumigimik
+Nakita mo sa IG ko, may gumigiling
+Nahumaling ka sa aking pagkanta
+Panay ang tanong mo sa 'kin, "Nasa'n ka?"
+Sabi ko, "Sa bahay," kahit gumigimik
+Nakita mo sa IG ko, may gumigiling
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino ('titino)
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino ('titino)
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom? Oh
+Gusto kitang makita kahit patago
+Sa tagpuan natin tayo magtagpo
+'Di ka nawawala sa puso at sistema ko
+Kaya pasensiya kung 'yong kuwento natin tinetema ko
+Nandiyan ka agad kapag kailangan ko
+Pero wala ako kapag kailangan mo
+Kasi madalas akong naka-airplane mode
+Ang sabi mo, "Baka kasama mo lang girlfriend mo"
+Naku, hindi, mas gusto lang kita na sabikin
+Para 'pag nagkita tayo, 'di bitin
+'Yong kalungkutan kong sakit, iyong gamutin
+"Mahal mo ba 'ko?" ang madalas mong tanungin
+'Di ko masagot, 'di pa sigurado
+Umibig ay takot, kasi natalo na 'ko
+Kaya madalas din ako na umiwas
+Mas gusto ko ay pera para mas lalong tumikas
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino ('titino)
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom? (YoungMLV got the sounds, bitch)
+</Text>
+      </ScrollView>
+);
+
+export default App;
